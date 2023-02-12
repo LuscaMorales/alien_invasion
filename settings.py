3bc4d80173537bf9b8f771c4f47ed33d5c9b8ae4
LucasNote <lucasmoraesbraga11@gmail.com>
2023-02-12 14:23:17 -0300

    principais

---
+++ settings.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Settings():
+    """Uma classe para armazenar todas as configuraçoes da Invasao Alienigena"""
+    def __init__(self):
+        self.screen_widht = 1200
+        self.screen_height = 650
+        self.bg_color = (230,230,230)
