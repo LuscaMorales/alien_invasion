70018b43f7425da7f9ff67047d9bc22edc22e715
LuscaMorales <120067440+LuscaMorales@users.noreply.github.com>
2023-02-12 23:27:52 -0300

    moving left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import game_fuction as gf
 import pygame
 from settings import Settings
 from ship import Ship
+from nave import Nave
 
 def run_game():
     pygame.init()
@@ -9,8 +10,10 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_widht,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(screen)
+    nave = Nave(screen)
     while True:
-        gf.check_events()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship,nave)
 
 run_game()
--- game_fuction.py
+++ game_fuction.py
@@ -1,12 +1,24 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                #move a espaconave para a direita
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = Falsev
 
-def update_screen(ai_settings,screen,ship):
+def update_screen(ai_settings,screen,ship,nave):
     screen.fill(ai_settings.bg_color)
+    nave.blitme1()
     ship.blitme()
     pygame.display.flip()
